b9f0be74ab099558bc438d5a434a2cbb9cb202c8
Josh <jonesjosh41@gmail.com>
2022-01-14 15:05:09 -0600

    Updates

--- static/js/39.107d47c2.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonp@kiba/interface"]=this["webpackJsonp@kiba/interface"]||[]).push([[39],{1859:function(i,a){}}]);
-//# sourceMappingURL=39.107d47c2.chunk.js.map
---
+++ static/js/40.fc02d78b.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonp@kiba/interface"]=this["webpackJsonp@kiba/interface"]||[]).push([[40],{2007:function(i,a){}}]);
+//# sourceMappingURL=40.fc02d78b.chunk.js.map
