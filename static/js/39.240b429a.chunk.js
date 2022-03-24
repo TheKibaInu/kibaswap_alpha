3da021bb4966be939782d35b8da27a50c0c85336
Josh <jonesjosh41@gmail.com>
2022-03-24 13:20:45 -0500

    Updates

--- static/js/39.240b429a.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonp@kiba/interface"]=this["webpackJsonp@kiba/interface"]||[]).push([[39],{1881:function(i,a){}}]);
-//# sourceMappingURL=39.240b429a.chunk.js.map
--- static/js/40.1dd1ed89.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonp@kiba/interface"]=this["webpackJsonp@kiba/interface"]||[]).push([[40],{1978:function(i,a){}}]);
-//# sourceMappingURL=40.1dd1ed89.chunk.js.map
