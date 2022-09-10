e42c1714266fb8131c21abafc165eeae7ba28029
nutz <nutz>
2022-09-10 06:55:35 -0500

    Updates

--- static/js/39.ccd7aa42.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonp@kiba/interface"]=this["webpackJsonp@kiba/interface"]||[]).push([[39],{2157:function(i,a){}}]);
-//# sourceMappingURL=39.ccd7aa42.chunk.js.map
---
+++ static/js/40.cdb32439.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonp@kiba/interface"]=this["webpackJsonp@kiba/interface"]||[]).push([[40],{2260:function(i,a){}}]);
+//# sourceMappingURL=40.cdb32439.chunk.js.map
